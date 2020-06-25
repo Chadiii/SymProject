004fe22e32eee9abac6f683ea180a99d4c9cd4c3
ARNAUD YARGA <arnaudyarga@gmail.com>
2020-06-25 11:35:04 +0100

    interfaces added

--- src/SYM/RestauBundle/Controller/DefaultController.php
+++ src/SYM/RestauBundle/Controller/DefaultController.php
@@ -9,18 +9,36 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    private $res = array();
+
+    function __construct(){
+        for($i=0;$i<=10;$i++){
+            $this->res[] = array("id"=>"a$i", "nom"=>"Pizza", "price"=>"50", "sup"=>array(array("nom"=>"ketchup", "price"=>"5"), array("nom"=>"sauce", "price"=>"8")), "img"=>"https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+            $this->res[] = array("id"=>"b$i", "nom"=>"Tacos", "price"=>"55", "sup"=>array(array("nom"=>"fromage", "price"=>"5"), array("nom"=>"frite", "price"=>"10")), "img"=>"https://img.cuisineaz.com/400x320/2019-04-17/i146583-tacos-poulet-curry.jpeg");
+            $this->res[] = array("id"=>"c$i", "nom"=>"Tagine", "price"=>"30", "sup"=>array(), "img"=>"https://img.cuisineaz.com/610x610/2017-06-22/i129611-tagine-de-courgettes-aux-tomates.jpeg");
+        }
+    }
     /**
      * @Route("/", name="index")
      */
     public function indexAction()
     {
-        $res = array();
-        for($i=0;$i<=10;$i++){
-            $res[] = array("nom"=>"Pizza", "price"=>"50", "sup"=>array(array("nom"=>"ketchup", "price"=>"5"), array("nom"=>"sauce", "price"=>"8")), "img"=>"https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
-            $res[] = array("nom"=>"Tacos", "price"=>"55", "sup"=>array(array("nom"=>"fromage", "price"=>"5"), array("nom"=>"frite", "price"=>"10")), "img"=>"https://img.cuisineaz.com/400x320/2019-04-17/i146583-tacos-poulet-curry.jpeg");
-            $res[] = array("nom"=>"Tagine", "price"=>"30", "sup"=>array(), "img"=>"https://img.cuisineaz.com/610x610/2017-06-22/i129611-tagine-de-courgettes-aux-tomates.jpeg");
+        return $this->render('@SYMRestau/Default/index.html.twig',["res"=>$this->res]);
+    }
+
+    /**
+     * @Route("/showMenu/{id}", methods={"GET","HEAD"}, name="showMenu")
+     */
+    public function showMenu(string $id)
+    {
+        $item = array();
+        foreach ($this->res as $el){
+            if($el["id"]==$id){
+                $item = $el;
+                break;
+            }
         }
-        return $this->render('@SYMRestau/Default/index.html.twig',["res"=>$res]);
+        return $this->render('@SYMRestau/Default/showMenu.html.twig',["item"=>$item]);
     }
 
     /**
@@ -54,13 +72,24 @@ class DefaultController extends Controller
      */
     public function manageMenu()
     {
-        $res = array();
-        for($i=0;$i<=10;$i++){
-            $res[] = array("nom"=>"Pizza", "price"=>"50", "sup"=>array(array("nom"=>"ketchup", "price"=>"5"), array("nom"=>"sauce", "price"=>"8")), "img"=>"https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
-            $res[] = array("nom"=>"Tacos", "price"=>"55", "sup"=>array(array("nom"=>"fromage", "price"=>"5"), array("nom"=>"frite", "price"=>"10")), "img"=>"https://img.cuisineaz.com/400x320/2019-04-17/i146583-tacos-poulet-curry.jpeg");
-            $res[] = array("nom"=>"Tagine", "price"=>"30", "sup"=>array(), "img"=>"https://img.cuisineaz.com/610x610/2017-06-22/i129611-tagine-de-courgettes-aux-tomates.jpeg");
-        }
-        return $this->render('@SYMRestau/Default/manageMenu.html.twig',["res"=>$res]);
+        return $this->render('@SYMRestau/Default/manageMenu.html.twig',["res"=>$this->res]);
+    }
+
+    /**
+     * @Route("/editMenu/{id}", methods={"GET","HEAD"}, name="editMenu")
+     */
+    public function editMenu(string $id)
+    {
+        
+        return $this->render('@SYMRestau/Default/editMenu.html.twig',);
+    }
+
+    /**
+     * @Route("/deleteMenu/{id}", methods={"GET","HEAD"}, name="deleteMenu")
+     */
+    public function deleteMenu(string $id)
+    {
+        return $this->redirectToRoute('manageMenu');
     }
 
     /**
@@ -77,6 +106,23 @@ class DefaultController extends Controller
     public function manageSupplement()
     {
         return $this->render('@SYMRestau/Default/manageSupplement.html.twig',);
+    }
+
+    /**
+     * @Route("/editSupplement/{id}", methods={"GET","HEAD"}, name="editSupplement")
+     */
+    public function editSupplement(string $id)
+    {
+        
+        return $this->render('@SYMRestau/Default/editSupplement.html.twig',);
+    }
+
+    /**
+     * @Route("/deleteSupplement/{id}", methods={"GET","HEAD"}, name="deleteSupplement")
+     */
+    public function deleteSupplement(string $id)
+    {
+        return $this->redirectToRoute('manageSupplement');
     }
 
     /**
